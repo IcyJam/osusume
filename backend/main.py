9392c90a13a184238879561c9851dae08663f9c6
SnowHawkeye <48864485+SnowHawkeye@users.noreply.github.com>
2025-07-03 11:57:16 +0200

    [BACKEND] Create file structure for databases

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,4 @@ import uvicorn
 
 if __name__ == "__main__":
     print("running main...")
-    uvicorn.run("app.routes:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.api.routes:app", host="0.0.0.0", port=8000, reload=True)
