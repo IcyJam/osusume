567ebc63fe974565c1f0188ec129f6c24bec6752
EmilieSalem <emilie59172@gmail.com>
2025-07-13 00:34:15 +0200

    [FRONTEND][REFACTO][#20] Make draggable window bound to screen + refacto in separate component

--- frontend/src/components/DraggableWindow.jsx
+++ frontend/src/components/DraggableWindow.jsx
@@ -1,27 +1,50 @@
-import { useDraggable } from '@dnd-kit/core';
+import { useState } from "react";
+import { DndContext, useDraggable } from "@dnd-kit/core";
+import { restrictToWindowEdges } from "@dnd-kit/modifiers";
 
-export function DraggableWindow({ id, position, children, label}) {
+export default function DraggableWindow({ id, children, label }) {
+	const [position, setPosition] = useState({ x: 100, y: 100 });					// Committed position, i.e. position before/after being dragged
 
-    const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({id,});       // Returns all the attributes necessary to make an object Draggable
-    
-    const x = position.x + (transform?.x || 0);         // Only handles the position during dragging, the end position is handled with setPosition
-    const y = position.y + (transform?.y || 0);                                    
+	// Drag context wraps the draggable element
+	return (
+		<DndContext
+			modifiers={[restrictToWindowEdges]}										// Makes it so that the draggable window can't go over the screen's borders
+			onDragEnd={({ delta }) => {												// Called when the user stops dragging
+			setPosition((p) => ({ x: p.x + delta.x, y: p.y + delta.y }));			// Adds the last drag delta to the committed coordinates
+		}}
+		>
+			<DraggableObject id={id} position={position} label={label}>
+				{children}
+			</DraggableObject>
+		</DndContext>
+	);
+}
 
-    const style = {
-        transform: `translate3d(${x}px, ${y}px, 0)`,
-        position: 'absolute',                           // Position defined relatively to parent
-        zIndex: isDragging ? 1000 : 1,                  // Takes the focus when being dragged
-    };
+function DraggableObject({ id, position, label, children }) {
+	const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({ id });			// Returns all the attributes necessary to make an object Draggable
 
-	// The object that has 'setNodeRef' and 'attributes' is Draggable, but only the part of the object that has 'listeners' can be used as a handle to move it
-    return (
-        <div ref={setNodeRef} {...attributes} style={style} className="rounded shadow-md bg-white border border-gray-300 absolute w-4xl">
+	// Compute style combining committed position + current drag transform (during drag)
+	const style = {
+		position: "absolute",
+		top: position.y,																				// Real layout position from which the delta is calculated
+		left: position.x,
+		transform: `translate3d(${transform ? transform.x : 0}px, ${transform ? transform.y : 0}px, 0)`,	// Position defined relatively to parent, changes while dragging
+		zIndex: isDragging ? 1000 : 1,																// Takes the focus when being dragged
+		cursor: 'grab',
+	};
+
+	return (
+		<div ref={setNodeRef} {...attributes} style={style} className="rounded shadow-md bg-white border border-gray-300 w-4xl absolute">
+
+			{/* Only this area is draggable */}
 			<div {...listeners} className="cursor-move bg-blue-600 text-white px-4 py-2 rounded-t">
 				{label}
 			</div>
-			<div className="p-4">							
+
+			{/* Other content inside — like form, input, etc. */}
+			<div className="p-4">
 				{children}
 			</div>
-        </div>
-  );
+		</div>
+	);
 }
